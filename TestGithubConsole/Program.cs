f3660a5e4edd1f44d8b7c98c3abdecfbed9163d4
Yves-Collet <Yves.Collet@loro.ch>
2025-02-13 13:46:48 +0100

    premier commit

---
+++ TestGithubConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TestGithubConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
