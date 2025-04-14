35ae44913384d117fd7ba6ab183f9f08bdc4bb1c
Collet Yves <Yves.Collet@loro.ch>
2025-04-14 09:20:51 +0200

    Update Program.cs

--- TestGithubConsole/Program.cs
+++ TestGithubConsole/Program.cs
@@ -7,6 +7,7 @@
             Console.WriteLine("Hello, World!");
             //ajout dans la branche 'ajout_writeline'
             Console.WriteLine("Hello, World! from ajout_writeline");
+            string x = "Hello World";
         }
 
         //ajout de cette méthode dans la brache 'main'
